b70aa0776227bf8caa0b18253f569b590052e597
임동혁 <idh1007@naver.com>
2023-10-11 00:08:26 +0900

    Feat: ELB 테스트를 위한 log 추가
    root 접속 시 log 를 통한 load balancing 확인

---
+++ src/main/java/com/windmeal/TestController.java
@@ -0,0 +1,19 @@
+package com.windmeal;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class TestController {
+
+    @GetMapping(value = "/")
+    public void test() {
+        log.info("log test");
+    }
+}
