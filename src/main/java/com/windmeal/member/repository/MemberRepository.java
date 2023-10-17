ed4252a6803ff1b33f009881e95c7510a65af1af
임동혁 <idh1007@naver.com>
2023-10-17 17:02:23 +0900

    Feat: memberRepository 추가
    - JpaRepository 로 MemberRepository 추가

---
+++ src/main/java/com/windmeal/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.windmeal.member.repository;
+
+import com.windmeal.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member,Long> {
+}
