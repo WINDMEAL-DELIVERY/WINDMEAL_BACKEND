d46f2e6cc4b576be4870b7f571e4ee2a58eba77e
임동혁 <idh1007@naver.com>
2023-10-24 19:50:53 +0900

    Refactor: repository 이름 수정

--- src/main/java/com/windmeal/store/repository/StoreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.windmeal.store.repository;
-
-import com.windmeal.store.domain.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StoreRepository extends JpaRepository<Store,Long> {
-}
