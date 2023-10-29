3bf79be9119522f598c6bf49f871b0c5f3f6e9f5
임동혁 <idh1007@naver.com>
2023-10-29 09:56:34 +0900

    Refactor : JpaRepository - @Repository 삭제

--- src/main/java/com/windmeal/store/repository/MenuRepository.java
+++ src/main/java/com/windmeal/store/repository/MenuRepository.java
@@ -4,7 +4,6 @@ import com.windmeal.store.domain.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> , MenuCustomRepository {
 
 
--- src/main/java/com/windmeal/store/repository/StoreRepository.java
+++ src/main/java/com/windmeal/store/repository/StoreRepository.java
@@ -4,7 +4,6 @@ import com.windmeal.store.domain.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
 }
