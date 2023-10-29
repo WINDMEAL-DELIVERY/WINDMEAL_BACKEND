f03bbcb642dc1916c1c9a0e60c8328b4ed2926e5
임동혁 <idh1007@naver.com>
2023-10-29 09:53:07 +0900

    Rename : JpaRepository 이름 변경

--- src/main/java/com/windmeal/store/repository/OptionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.windmeal.store.repository;
-
-import com.windmeal.store.dto.request.OptionCreateRequest.OptionSpecRequest;
-import java.util.List;
-
-public interface OptionRepository {
-
-  void createOptionSpecs(List<OptionSpecRequest> optionSpecifications, Long option_group_id);
-}
