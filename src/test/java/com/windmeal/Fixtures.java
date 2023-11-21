dbec3826874383c32b26d44a732731e84cea2daf
임동혁 <idh1007@naver.com>
2023-11-21 20:29:12 +0900

    Refactor : 테스트 코드를 위한 객체 생성 코드 추가

--- src/test/java/com/windmeal/Fixtures.java
+++ src/test/java/com/windmeal/Fixtures.java
@@ -4,6 +4,15 @@ import com.windmeal.member.domain.Member;
 import com.windmeal.member.domain.Member.MemberBuilder;
 import com.windmeal.order.domain.Order;
 import com.windmeal.order.domain.Order.OrderBuilder;
+import com.windmeal.store.domain.Menu;
+import com.windmeal.store.domain.Menu.MenuBuilder;
+import com.windmeal.store.domain.OptionGroup;
+import com.windmeal.store.domain.OptionGroup.OptionGroupBuilder;
+import com.windmeal.store.domain.OptionSpecification;
+import com.windmeal.store.domain.OptionSpecification.OptionSpecificationBuilder;
+import com.windmeal.store.domain.Store;
+import com.windmeal.store.domain.Store.StoreBuilder;
+import java.util.Arrays;
 
 public class Fixtures {
 
@@ -15,7 +24,31 @@ public class Fixtures {
   }
   public static MemberBuilder aMember(){
     return Member.builder()
-        .id(1L);
+        ;
   }
 
+  public static StoreBuilder aStore() {
+    return Store.builder()
+        .name("오겹돼지");
+  }
+
+  public static MenuBuilder aMenu() {
+    return Menu.builder()
+        .optionGroups(Arrays.asList(aOptionGroup().build()))
+        ;
+  }
+
+  public static OptionGroupBuilder aOptionGroup() {
+    return OptionGroup.builder()
+        .id(1L)
+        .isMultipleOption(true)
+        .isEssentialOption(true)
+        .optionSpecifications(Arrays.asList(aOptionSpecification().build(),
+            aOptionSpecification().build()))
+        ;
+  }
+
+  public static OptionSpecificationBuilder aOptionSpecification() {
+    return OptionSpecification.builder();
+  }
 }
