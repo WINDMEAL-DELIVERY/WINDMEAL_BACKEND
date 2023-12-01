791befb329927ae74a7b99c4b6aa09ed2e1a9cfb
Owen-Choi <oldstyle482@gmail.com>
2023-12-01 13:49:38 +0900

    refactor : response header 관련 filter 수정

--- src/main/java/com/windmeal/global/security/impl/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/windmeal/global/security/impl/filter/JwtAuthenticationFilter.java
@@ -57,6 +57,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         if (StringUtils.hasText(decrypt) && tokenProvider.validateToken(decrypt)) {
           Authentication authenticated = tokenProvider.getAuthentication(decrypt);
           SecurityContextHolder.getContext().setAuthentication(authenticated);
+          response.setCharacterEncoding("utf-8");
+          response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+          response.setHeader("Access-Control-Allow-Credentials", "true");
+          response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE,PATCH");
+          response.setHeader("Access-Control-Max-Age", "3600");
+          response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me,token");
         }
       }
     } catch (Exception e) {
