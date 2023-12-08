0157af46297d059bfe9c0de09da6e6a95fe3bdf4
임동혁 <idh1007@naver.com>
2023-12-08 20:13:09 +0900

    Feat : Event 추가

---
+++ src/main/java/com/windmeal/model/event/EventPublisher.java
@@ -0,0 +1,19 @@
+package com.windmeal.model.event;
+
+
+import org.springframework.context.ApplicationEventPublisher;
+
+public class EventPublisher {
+
+    private static ApplicationEventPublisher eventPublisher;
+
+    public static void setPublisher(ApplicationEventPublisher publisher){
+        EventPublisher.eventPublisher = publisher;
+    }
+
+    public static void raise(Event event){
+        if(eventPublisher != null){
+            eventPublisher.publishEvent(event);
+        }
+    }
+}
