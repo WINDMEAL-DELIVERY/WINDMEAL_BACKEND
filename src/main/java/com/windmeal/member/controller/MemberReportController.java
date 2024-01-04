8c7d8d6f7d1e2e6caf3be2e4ba1e145ace66691c
임동혁 <idh1007@naver.com>
2024-01-04 19:57:08 +0900

    Refactor : RequestBody 추가

--- src/main/java/com/windmeal/member/controller/MemberReportController.java
+++ src/main/java/com/windmeal/member/controller/MemberReportController.java
@@ -18,6 +18,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,9 +38,8 @@ public class MemberReportController {
       @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없습니다."),
   })
   @PostMapping("/report")
-  public ResultDataResponseDTO createMemberReport(MemberReportCreateRequest request){
+  public ResultDataResponseDTO createMemberReport(@RequestBody MemberReportCreateRequest request){
     Long currentMemberId = SecurityUtil.getCurrentMemberId();
-
     memberReportService.createMemberReport(request.toServiceDto(currentMemberId));
     return ResultDataResponseDTO.empty(ErrorCode.CREATED);
   }
