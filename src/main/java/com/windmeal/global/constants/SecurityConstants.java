17d1c8c14bb8176ed3f27de1891718f6b32a7bf6
Owen-Choi <oldstyle482@gmail.com>
2024-04-01 20:16:03 +0900

    Fix : 리다이렉트 uri 배포 주소로 수정

--- src/main/java/com/windmeal/global/constants/SecurityConstants.java
+++ src/main/java/com/windmeal/global/constants/SecurityConstants.java
@@ -4,9 +4,9 @@ public class SecurityConstants {
 
     public static String ANONYMOUSUSER = "anonymousUser";
     // 모두 yml로 옮길 예정
-    public static String ERROR_REDIRECT = "http://localhost:3000/error";
-    public static String DEFAULT_OAUTH_REDIRECT = "http://localhost:3000/main";
-    public static String NICKNAME_REDIRECT = "http://localhost:3000/register";
+    public static String ERROR_REDIRECT = "https://windmeal.co.kr/error";
+    public static String DEFAULT_OAUTH_REDIRECT = "https://windmeal.co.kr";
+    public static String NICKNAME_REDIRECT = "https://windmeal.co.kr/register";
     public static final String MISSING_USER_INFO_URI_ERROR_CODE = "missing_user_info_uri";
     public static final String MISSING_USER_NAME_ATTRIBUTE_ERROR_CODE = "missing_user_name_attribute";
     public static final String INVALID_USER_INFO_RESPONSE_ERROR_CODE = "invalid_user_info_response";
