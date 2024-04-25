33cbc46c287b0cda96ca246b65f4c98b6d4c8060
Owen-Choi <oldstyle482@gmail.com>
2024-04-25 16:26:36 +0900

    !HOTFIX : 사용자 신고 API 관련 빌드 오류 원인 수정

--- src/main/java/com/windmeal/member/dto/request/MemberReportCreateRequest.java
+++ src/main/java/com/windmeal/member/dto/request/MemberReportCreateRequest.java
@@ -15,7 +15,9 @@ public class MemberReportCreateRequest {
   @Schema(description = "신고 유저 ID", example = "1")
   private Long reportedId;
 
-  @Schema(description = "신고 내용", example = "신고 내용")
+  @Schema(description = "신고 내용", example = "신의한컵 30인분 25만원 부탁했는데 먹튀했어요")
   private String content;
 
+  @Schema(description = "신고 제목", example = "먹튀범 \"나는 동혁\" 신고합니다.")
+  private String title;
 }
