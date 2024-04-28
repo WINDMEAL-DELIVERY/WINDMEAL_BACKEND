89957b1d2d339a49189c02c9b4edcabb99f25468
Owen-Choi <oldstyle482@gmail.com>
2024-04-28 16:34:39 +0900

    fix : 사용자 차단 DTO 필드 수정

--- src/main/java/com/windmeal/member/dto/request/BlackListCreateRequest.java
+++ src/main/java/com/windmeal/member/dto/request/BlackListCreateRequest.java
@@ -1,5 +1,6 @@
 package com.windmeal.member.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,7 +10,9 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(title = "사용자 차단 요청")
 public class BlackListCreateRequest {
 
-  private String email;
+  @Schema(description = "차단할 사용자의 Id", example = "1")
+  private Long id;
 }
