42adf95b10a9c197a8e0b8a26f12644895b2aa27
Owen-Choi <oldstyle482@gmail.com>
2024-06-17 14:20:26 +0900

    feat : 채팅방 존재 유무 확인 - 상대방의 알람 토큰 추가

--- src/main/java/com/windmeal/chat/dto/response/ChatRoomExistsResponse.java
+++ src/main/java/com/windmeal/chat/dto/response/ChatRoomExistsResponse.java
@@ -8,8 +8,14 @@ import lombok.NoArgsConstructor;
 @Getter
 public class ChatRoomExistsResponse {
     private final String chatroomId;
+    private final String opponentAlarmToken;
 
-    public ChatRoomExistsResponse(String chatroomId) {
+    public ChatRoomExistsResponse(String chatroomId, String opponentAlarmToken) {
         this.chatroomId = chatroomId;
+        this.opponentAlarmToken = opponentAlarmToken;
+    }
+
+    public static ChatRoomExistsResponse nullInstance() {
+        return new ChatRoomExistsResponse(null, null);
     }
 }
